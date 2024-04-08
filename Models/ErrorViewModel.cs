f194479a286376dfda49d07796d71efb480710e0
Jennifer Peterson <jennpeterson986@gmail.com>
2024-04-07 20:52:15 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DDVTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
