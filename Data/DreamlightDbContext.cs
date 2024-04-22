92455a9bf01bdc572abed5f446bc3d9073fa60d6
Jennifer Peterson <jennpeterson986@gmail.com>
2024-04-22 13:38:43 -0700

    Renamed DbContext. Added grid to home

---
+++ Data/DreamlightDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.PortableExecutable;
+
+namespace DDVTracker.Data
+{
+    public class DreamlightDbContext : IdentityDbContext
+    {
+        public DreamlightDbContext(DbContextOptions<DreamlightDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Character>
+    }
+}
