0d86f6f5c4d46216afbbd2d1c3c551c530296be1
Jennifer Peterson <jennpeterson986@gmail.com>
2024-04-27 16:03:36 -0700

    Added seed data and Initial Migration

--- Data/DreamlightDbContext.cs
+++ Data/DreamlightDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DDVTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.PortableExecutable;
 
@@ -10,5 +11,23 @@ namespace DDVTracker.Data
         {
         }
 
+        public DbSet<GameVersion> GameVersion { get; set; }
+        public DbSet<Character> Characters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Call the base class's OnModelCreating method
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<GameVersion>().HasData(
+                new GameVersion { GameVersionId = 1, GameVersionName = "Base Game"},
+                new GameVersion { GameVersionId = 2, GameVersionName = "A Rift In Time"}
+            );
+
+            modelBuilder.Entity<Character>().HasData(
+                new Character { CharacterId = 1, GameVersionId = 1, CharacterName = "Mickey Mouse", CharacterLevel = 1 },
+                new Character { CharacterId = 2, GameVersionId = 2, CharacterName = "Rapunzel", CharacterLevel = 5 }
+            );
+        }
     }
 }
