369f1e2bdd7588b0c64c23967475b142592d0d42
Jennifer Peterson <jennpeterson986@gmail.com>
2024-06-12 23:59:16 -0700

    Added Ingredients class

---
+++ Models/Ingredient.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace DDVTracker.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int IngredientId { get; set; }
+
+        /// <summary>
+        /// Foreign key for GameVersionId of the game the character is included in
+        /// Navigation property for the GameVersion
+        /// </summary>
+        public int GameVersionId { get; set; }
+        public GameVersion? GameVersion { get; set; }
+        public string IngredientName { get; set; }
+
+        /// <summary>
+        /// Type of ingredient i.e. fruit, vegetable, fish, etc.
+        /// </summary>
+        public string? IngredientCategory { get; set; }
+
+        /// <summary>
+        /// Price to buy from stall
+        /// </summary>
+        public int? BuyPrice { get; set; }
+
+        /// <summary>
+        /// Price to buy from stall
+        /// </summary>
+        public int? SellsFor { get; set; }
+
+        /// <summary>
+        /// Energy gained from eating
+        /// </summary>
+        public int? Energy { get; set; }
+        /// <summary>
+        /// Time to grow in real time
+        /// </summary>
+        public string? GrowTime { get; set; }
+
+        /// <summary>
+        /// How many times it needs to be watered
+        /// </summary>
+        public int? Water { get; set; }
+
+        /// <summary>
+        /// Yield per harvest of one crop
+        /// </summary>
+        public int? Yield { get; set; }
+
+        /// <summary>
+        /// Location where the ingredient can be found
+        /// </summary>
+        public int? LocationId { get; set; }
+
+        /// <summary>
+        /// How to obtain the ingredient
+        /// </summary>
+        public string? Method { get; set; }
+
+        /// <summary>
+        /// Additional info for the ingredient
+        /// i.e. quest to be completed first
+        /// </summary>
+        public string? Notes { get; set; }
+    }
+}
