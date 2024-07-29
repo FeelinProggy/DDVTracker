9adac6fc990505a993f9f723a41a44110b11745a
Jennifer Peterson <jennpeterson986@gmail.com>
2024-07-28 22:34:59 -0400

    Created MealIngredient to handle Many to Many relationship of meals and Ingredients

---
+++ Models/MealIngredient.cs
@@ -0,0 +1,10 @@
+﻿namespace DDVTracker.Models
+{
+    public class MealIngredient
+    {
+        public int MealID { get; set; }
+        public Meal Meal { get; set; }
+        public int IngredientID { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
